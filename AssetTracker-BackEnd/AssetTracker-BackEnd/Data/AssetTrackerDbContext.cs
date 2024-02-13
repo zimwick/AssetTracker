df1e99a92061019ee5650d15a19681ce55c7156d
LIGHTJ60 <jligh43@wgu.edu>
2024-02-13 13:34:31 -0700

    seeded demo data into assets table

--- AssetTracker-BackEnd/AssetTracker-BackEnd/Data/AssetTrackerDbContext.cs
+++ AssetTracker-BackEnd/AssetTracker-BackEnd/Data/AssetTrackerDbContext.cs
@@ -10,5 +10,41 @@ namespace AssetTracker_BackEnd.Data
         }
 
         public DbSet<Asset> Assets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Asset>().HasData(
+                new Asset
+                {
+                    Id = 1,
+                    Name = "Macbook Pro",
+                    Make = "Apple",
+                    Model = "M2 Pro",
+                    Location = "Main Headquarters",
+                    SerialNumber = "X3742GH59",
+                    PricePaid = 2500.50m,
+                    DatePurchased = new DateOnly(2024, 2, 13),
+                    WarrantyExpiration = new DateOnly(2025, 2, 13),
+                    OwnerFirstName = "Jay",
+                    OwnerLastName = "Lightfoot"
+                },
+                new Asset
+                {
+                    Id = 2,
+                    Name = "Tablet",
+                    Make = "Acer",
+                    Model = "Model Pro",
+                    Location = "HR Building Room 12",
+                    SerialNumber = "Z3742534TGH",
+                    PricePaid = 450.25m,
+                    DatePurchased = new DateOnly(2023, 1, 25),
+                    WarrantyExpiration = new DateOnly(2024, 1, 25),
+                    OwnerFirstName = "Cathie",
+                    OwnerLastName = "Albridge"
+                }
+
+             );
+        }
     }
 }
---
+++ AssetTracker-BackEnd/AssetTracker-BackEnd/Migrations/20240213202835_SeededAssets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AssetTracker_BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeededAssets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Assets",
+                columns: new[] { "Id", "DatePurchased", "Location", "Make", "Model", "Name", "OwnerFirstName", "OwnerLastName", "PricePaid", "SerialNumber", "WarrantyExpiration" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(2024, 2, 13), "Main Headquarters", "Apple", "M2 Pro", "Macbook Pro", "Jay", "Lightfoot", 2500.50m, "X3742GH59", new DateOnly(2025, 2, 13) },
+                    { 2, new DateOnly(2023, 1, 25), "HR Building Room 12", "Acer", "Model Pro", "Tablet", "Cathie", "Albridge", 450.25m, "Z3742534TGH", new DateOnly(2024, 1, 25) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}

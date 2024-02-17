42fa97c5b24566c11ffda994142ae5e718f72c26
jaylightfoot <jligh43@wgu.edu>
2024-02-16 20:47:21 -0700

    changed asset table structure

--- AssetTracker-FrontEnd/src/components/AssetTable.jsx
+++ AssetTracker-FrontEnd/src/components/AssetTable.jsx
@@ -4,52 +4,44 @@ import styles from "./AssetTable.module.css";
 export default function AssetTable({ response, setShowDetails }) {
   const [editRowId, setEditRowId] = useState(null);
 
-  // Handler to toggle edit mode for a row
+  // Assuming all items in `response` have the same structure
+  // Exclude 'id' from the columns
+  const columns =
+    response.length > 0
+      ? Object.keys(response[0]).filter((key) => key !== "id")
+      : [];
+
   const handleRowClick = (item) => {
     const newEditRowId = item.id === editRowId ? null : item.id;
     setEditRowId(newEditRowId);
-
-    // Check if we're setting a row to edit mode, then pass the item to setShowDetails
-    if (newEditRowId !== null) {
-      setShowDetails(item); // Pass the entire item object
-    } else {
-      setShowDetails(null); // Reset or pass a default state when no row is in edit mode
-    }
+    setShowDetails(newEditRowId ? item : null);
   };
 
   return (
     <table>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Make</th>
-          <th>Model</th>
-          <th>Location</th>
-          <th>Serial Number</th>
-          <th>Price Paid</th>
-          <th>Date Purchased</th>
-          <th>Warranty Expiration</th>
-          <th>Owner First Name</th>
-          <th>Owner Last Name</th>
+          {columns.map((column) => (
+            <th key={column}>
+              {column
+                .replace(/([A-Z])/g, " $1")
+                .trim()
+                .replace(/^./, (str) => str.toUpperCase())}{" "}
+              {/* Convert camelCase to Title Case */}
+            </th>
+          ))}
         </tr>
       </thead>
       <tbody>
         {response.map((item) => (
           <tr
-            className={`${styles.tr}`}
+            className={styles.tr}
             key={item.id}
             onClick={() => handleRowClick(item)}
           >
-            <td>{item.name}</td>
-            <td>{item.make}</td>
-            <td>{item.model}</td>
-            <td>{item.location}</td>
-            <td>{item.serialNumber}</td>
-            <td>{item.pricePaid}</td>
-            <td>{item.datePurchased}</td>
-            <td>{item.warrantyExpiration}</td>
-            <td>{item.ownerFirstName}</td>
-            <td>{item.ownerLastName}</td>
+            {columns.map((column) => (
+              <td key={`${item.id}-${column}`}>{item[column]}</td>
+            ))}
           </tr>
         ))}
       </tbody>

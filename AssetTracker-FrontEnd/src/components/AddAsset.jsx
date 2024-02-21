a9867702da613d10a22aa02e7a2dce8998c67a0a
jaylightfoot <jligh43@wgu.edu>
2024-02-20 18:01:10 -0700

    updated price formatting

--- AssetTracker-FrontEnd/src/components/AddAsset.jsx
+++ AssetTracker-FrontEnd/src/components/AddAsset.jsx
@@ -17,6 +17,44 @@ export default function DashboardForm({
   const [ownerFirstName, setOwnerFirstName] = useState("");
   const [ownerLastName, setOwnerLastName] = useState("");
   const [priceError, setPriceError] = useState(false);
+  const [dateError, setDateError] = useState(false);
+
+  const validateDates = (newDatePurchased, newWarrantyExpiration) => {
+    // Convert to date objects for comparison
+    const purchaseDate = new Date(newDatePurchased);
+    const expirationDate = new Date(newWarrantyExpiration);
+
+    // Validate that neither date precedes the other
+    if (
+      newDatePurchased &&
+      newWarrantyExpiration &&
+      purchaseDate > expirationDate
+    ) {
+      setDateError(true);
+      return false;
+    } else {
+      setDateError(false);
+      return true;
+    }
+  };
+
+  const handleDatePurchasedChange = (e) => {
+    const newDatePurchased = e.target.value;
+    if (validateDates(newDatePurchased, warrantyExpiration)) {
+      setDatePurchased(newDatePurchased);
+    }
+  };
+
+  const handleWarrantyExpirationChange = (e) => {
+    const newWarrantyExpiration = e.target.value;
+    // Check and convert an empty string to null before validation
+    const adjustedWarrantyExpiration =
+      newWarrantyExpiration === "" ? null : newWarrantyExpiration;
+
+    if (validateDates(DatePurchased, adjustedWarrantyExpiration)) {
+      setWarrantyExpiration(adjustedWarrantyExpiration);
+    }
+  };
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -32,9 +70,9 @@ export default function DashboardForm({
       ownerFirstName,
       ownerLastName,
     };
-    if (ValidateDollarAmmount(pricePaid) === false) {
+    if (priceError == false && dateError == false) {
       setPriceError(true);
-    } else {
+
       await postData(newAsset);
       toggleFormVisibility();
       await getData(sessionStorage.getItem("token"));
@@ -145,9 +183,18 @@ export default function DashboardForm({
             id="pricePaid"
             type="text"
             value={pricePaid}
-            onChange={(e) => setPricePaid(e.target.value)}
+            onChange={(e) => {
+              const value = e.target.value;
+              // Set the pricePaid state to the new value
+              setPricePaid(value);
+              // Check if the new value is a valid dollar amount and update priceError state
+              const isValid = ValidateDollarAmmount(value) || value === ""; // Allow empty string to clear the error
+              setPriceError(!isValid);
+            }}
             placeholder="Enter price paid"
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+            className={`mt-1 block w-full px-3 py-2 bg-white border ${
+              priceError ? "border-red-500" : "border-gray-300"
+            } rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
           />
         </div>
         {/* Date Purchased */}
@@ -162,27 +209,28 @@ export default function DashboardForm({
             id="datePurchased"
             type="date"
             value={DatePurchased}
-            onChange={(e) => setDatePurchased(e.target.value)}
+            onChange={handleDatePurchasedChange}
             className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
-        {/* Warranty Expiration */}
         <div>
           <label
             htmlFor="warrantyExpiration"
-            className="block text-sm font-medium text-gray-700"
+            className={
+              dateError
+                ? "block text-sm font-medium text-red-700"
+                : "block text-sm font-medium text-gray-700"
+            }
           >
-            Warranty Expiration (optional)
+            Warranty Expiration (optional){" "}
+            {dateError &&
+              "Error: Date Purchased must be earlier than Warranty Expiration"}
           </label>
           <input
             id="warrantyExpiration"
             type="date"
             value={warrantyExpiration}
-            onChange={(e) =>
-              setWarrantyExpiration(
-                e.target.value === "" ? null : e.target.value
-              )
-            }
+            onChange={handleWarrantyExpirationChange}
             className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
--- AssetTracker-FrontEnd/src/components/AssetDetail.jsx
+++ AssetTracker-FrontEnd/src/components/AssetDetail.jsx
@@ -240,7 +240,7 @@ export default function AssetDetail({ showDetails, setShowDetails, getData }) {
           <input
             id="pricePaid"
             type="text"
-            value={pricePaid}
+            value={Number(pricePaid).toFixed(2)}
             onChange={(e) => {
               const value = e.target.value;
               // Set the pricePaid state to the new value
